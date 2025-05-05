413f3b7f06ffd8cd6141b5cf6d09f869b1c95543
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-05-05 11:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+	function getFormvalue() {
+      const form = document.getElementById("nameForm");
+
+      // Trim to remove extra spaces
+      const firstName = form.fname.value.trim();
+      const lastName = form.lname.value.trim();
+
+      // Alert full name
+      alert(`${firstName} ${lastName}`);
+
+      return false; // Prevent form submission
 
 }
